7cc9f47af01ef35502bda91ea3ddb1761401fb2f
LuisCR91 <luisalfredo.rodriguez.cespedes@ucreativa.com>
2021-11-24 21:21:31 -0600

    Commit de prueba, abriendo el sitio solicitado

--- Greenslate code challenge/UnitTest1.cs
+++ Greenslate code challenge/UnitTest1.cs
@@ -1,18 +1,28 @@
 using NUnit.Framework;
-
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 namespace Greenslate_code_challenge
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        IWebDriver webDriver;
+
+        [OneTimeSetUp]
+        public void StartChrome()
         {
+            webDriver = new ChromeDriver(".");
         }
 
         [Test]
         public void Test1()
         {
+            webDriver.Url = "https://demo.opencart.com/";
             Assert.Pass();
+        }
+        [OneTimeTearDown]
+        public void CloseTest()
+        {
+            webDriver.Close();
         }
     }
 }
