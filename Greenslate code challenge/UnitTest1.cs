c9486cccb9ec7491765399ccd606b38cb6eb0616
LuisCR91 <luisalfredo.rodriguez.cespedes@ucreativa.com>
2021-11-24 21:01:50 -0600

    Agregar archivos de proyecto.

---
+++ Greenslate code challenge/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Greenslate_code_challenge
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
